e8d06953faf8e793483c687cc5459e5e3ceed5c7
bebopvinh <vinhdo07@gmail.com>
2019-05-17 20:20:19 -0700

    Workout can submit multiple skills -- messy as hell

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -2,15 +2,17 @@ class WorkoutsController < ApplicationController
 
   def new
     @workout = Workout.new
-    18.times{@workout.skills.build}
-    @skills = Skill.all + [Skill.new(name: "none")]
-    @targets = @skills.map{|skill| skill.target}.uniq - [nil]
+
   end
 
   def create
     @workout = Workout.new
     binding.pry
     redirect_to workouts_path(@workout)
+    @skills = params[:workout][:skills]
+    @workout.skills << @skills
+    @workout.save
+    redirect
   end
 
   def show
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,6 @@
 class Workout < ApplicationRecord
   has_many :workout_skills
   has_many :skills, through: :workout_skills
-  accepts_nested_attributes_for :skills 
+  accepts_nested_attributes_for :workout_skills 
+  accepts_nested_attributes_for :skills
 end
