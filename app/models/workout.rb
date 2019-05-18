e86fb24e6c03de58acbe57d1f5d1c549c8d07f21
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-18 00:19:36 -0700

    created workout date method

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,10 @@
 class Workout < ApplicationRecord
   has_many :workout_skills
   has_many :skills, through: :workout_skills
-  accepts_nested_attributes_for :skills 
+  accepts_nested_attributes_for :workout_skills
+  accepts_nested_attributes_for :skills
+
+  def date
+    self.created_at.to_formatted_s(:long_ordinal)[0...-6]
+  end
 end
