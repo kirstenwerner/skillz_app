fd4a8dee0dc054089363c57ac9b10def640602e3
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-18 00:17:30 -0700

    workout controller methods and show view

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,5 +1,9 @@
 class WorkoutsController < ApplicationController
 
+  def index
+    @workouts = Workout.all
+  end
+
   def new
     @workout = Workout.new
     18.times{@workout.skills.build}
@@ -9,10 +13,31 @@ class WorkoutsController < ApplicationController
 
   def create
     @workout = Workout.new
-    binding.pry
-    redirect_to workouts_path(@workout)
+    @workout.coach_id =
+    @skills = []
+    params[:workout].values.each do |skill_hash|
+      skill_hash.each do |skill, id|
+        @skills << Skill.find(id)
+      end
+    end
+    @workout.skills << @skills
+    if @workout.save
+      binding.pry
+      redirect_to workout_path(@workout)
+    else
+      render :new
+    end
   end
 
   def show
+    @workout = Workout.find(params[:id])
+    @skills = @workout.skills
+    @targets = @skills.map{|skill| skill.target}.uniq
+  end
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:id, :coach_id, skill_attributes:[:name, :description, :target])
   end
 end
