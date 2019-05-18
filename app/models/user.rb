afb818c3bfc478a07f0f02e79381ae4cc35f5865
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-18 04:00:52 -0700

    updated associations for user and workout models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
 
   has_many :user_skills
   has_many :skills, through: :user_skills
+  
+  has_many :workout_users
+  has_many :workouts, through: :workout_users
+
 
   def self.from_omniauth(auth)
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,10 +1,21 @@
 class Workout < ApplicationRecord
   has_many :workout_skills
   has_many :skills, through: :workout_skills
+
   accepts_nested_attributes_for :workout_skills
   accepts_nested_attributes_for :skills
 
+  has_many :workout_users
+  has_many :users, through: :workout_users
+  
+  accepts_nested_attributes_for :workout_users
+  accepts_nested_attributes_for :users
+
   def date
     self.created_at.to_formatted_s(:long_ordinal)[0...-6]
+  end
+
+  def self.today
+    @workout = Workout.where({ created_at: (Time.now.midnight - 1.day)..Time.now.midnight })
   end
 end
