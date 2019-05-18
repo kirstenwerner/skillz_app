b6d262698a763b1b77213c5b21e036536f5b2c06
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-18 04:00:18 -0700

    added filters, added conditionals to create, and created helper methed in workouts_controller

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,28 +1,33 @@
 class WorkoutsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :coach?, except: [:index, :show]
 
   def index
     @workouts = Workout.all
   end
 
   def new
-    @workout = Workout.new
-    18.times{@workout.skills.build}
-    @skills = Skill.all + [Skill.new(name: "none")]
-    @targets = @skills.map{|skill| skill.target}.uniq - [nil]
+    if Workout.today
+      redirect_to root_path, alert: "A workout has already been created for today!"
+    else
+      @workout = Workout.new
+      18.times{@workout.skills.build}
+      @skills = Skill.all + [Skill.new(name: "none")]
+      @targets = @skills.map{|skill| skill.target}.uniq - [nil]
+    end
   end
 
   def create
     @workout = Workout.new
-    @workout.coach_id =
+    @workout.coach_id = current_user.id
     @skills = []
     params[:workout].values.each do |skill_hash|
       skill_hash.each do |skill, id|
-        @skills << Skill.find(id)
+        @skills << Skill.find(id) unless id == ""
       end
     end
     @workout.skills << @skills
     if @workout.save
-      binding.pry
       redirect_to workout_path(@workout)
     else
       render :new
@@ -33,6 +38,12 @@ class WorkoutsController < ApplicationController
     @workout = Workout.find(params[:id])
     @skills = @workout.skills
     @targets = @skills.map{|skill| skill.target}.uniq
+  end
+
+  def coach?
+    unless current_user.coach
+      redirect_to root_path, alert: "You must be a coach to create or edit workouts!"
+    end
   end
 
   private
