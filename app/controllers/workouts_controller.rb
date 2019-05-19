c7312d1545a8a70b673408a9128410f779e5936b
kirstenwerner <kirstenmwerner@gmail.com>
2019-05-19 01:37:54 -0700

    updated new workout form, method, and view

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -7,27 +7,36 @@ class WorkoutsController < ApplicationController
   end
 
   def new
-    if Workout.today
-      redirect_to root_path, alert: "A workout has already been created for today!"
-    else
+    # if Workout.today
+    #   redirect_to root_path, alert: "A workout has already been created for today!"
+    # else
       @workout = Workout.new
       18.times{@workout.skills.build}
       @skills = Skill.all + [Skill.new(name: "none")]
       @targets = @skills.map{|skill| skill.target}.uniq - [nil]
-    end
+    # end
   end
 
   def create
     @workout = Workout.new
     @workout.coach_id = current_user.id
-    @skills = []
+    skills = []
+    skill_work = []
     params[:workout].values.each do |skill_hash|
-      skill_hash.each do |skill, id|
-        @skills << Skill.find(id) unless id == ""
+      skill_hash.each do |key, value|
+        if !key.include? "work"
+          skills << Skill.find(value) unless value.to_i == 0
+        else
+          skill_work << value unless value == ""
+        end
       end
     end
-    @workout.skills << @skills
+    @workout.skills << skills
     if @workout.save
+      @workout.skills.each_with_index do |skill, i|
+        @workout.workout_skills[i].work = skill_work[i]
+      end
+      @workout.save
       redirect_to workout_path(@workout)
     else
       render :new
@@ -37,7 +46,18 @@ class WorkoutsController < ApplicationController
   def show
     @workout = Workout.find(params[:id])
     @skills = @workout.skills
+    @work = []
+
+    @workout.workout_skills.each do |ws|
+      @work << ws.work
+    end
     @targets = @skills.map{|skill| skill.target}.uniq
+  end
+
+  def edit
+    @workout = Workout.find(params[:id])
+    @skills = Skill.all + [Skill.new(name: "none")]
+    @targets = @skills.map{|skill| skill.target}.uniq - [nil]
   end
 
   def coach?
