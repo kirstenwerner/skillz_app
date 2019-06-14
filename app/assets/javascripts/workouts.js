0b51047aeb9d80df09c2f6698edcb47f5aadac2d
kirstenwerner <kirstenmwerner@gmail.com>
2019-06-14 16:46:03 -0700

    workout constructor method and refactoring

--- app/assets/javascripts/workouts.js
+++ app/assets/javascripts/workouts.js
@@ -1,28 +1,21 @@
-// document.on('turbolinks:load', function() {
-// const collection = document.getElementByClassName("workouts-js")
-//
-// debugger
-// })
-
 document.addEventListener("turbolinks:load", function() {
 
   const body = document.querySelector("body")
+  getWorkouts()
   body.addEventListener("click", function(e) {
     if (e.target.className === "workout-js"){
       const workoutId = e.target.dataset.id
-      fetch(`http://localhost:3000/workouts/${workoutId}.json`)
-      .then(resp => resp.json())
-      .then(data => {
+      let workout = findWorkoutById(workoutId)
         const show = document.getElementById("workout-show")
         let div = ""
         let i
-        for (i = 0; i < data.skills.length; i++) {
-          div += `<p>${data.skills[i].name}: ${data.workout_skills[i].work}</p>`
+        for (i = 0; i < workout.skills.length; i++) {
+          div += `<p>${workout.skills[i].name}: ${workout.workout_skills[i].work}</p>`
         }
         show.innerHTML = '<br>' + div + '<br>'
-      })
     }
   })
+
   $(function () {
     $('#new_skills').submit(function(event) {
       event.preventDefault()
@@ -39,4 +32,35 @@ document.addEventListener("turbolinks:load", function() {
       })
     })
   })
+
+  function getWorkouts() {
+    fetch(`http://localhost:3000/workouts.json`)
+    .then(resp => resp.json())
+    .then(data => {
+      data.map(workout => {
+        const newWorkout = new Workout(workout)
+      })
+    })
+  }
+
+  allWorkouts = []
+
+  class Workout {
+    constructor(obj) {
+      this.id = obj.id
+      this.coach_id = obj.coach_id
+      this.skills = obj.skills
+      this.workout_skills = obj.workout_skills
+      allWorkouts.push(this)
+    }
+  }
+
+  function findWorkoutById(workoutId) {
+    for (var i = 0; i < allWorkouts.length; i++) {
+      if (allWorkouts[i].id == workoutId) {
+        return allWorkouts[i];
+      }
+    }
+  }
+
 });
