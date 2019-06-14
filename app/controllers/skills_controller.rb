898157af57570475ecb8169b1a19309cc9ff39eb
kirstenwerner <kirstenmwerner@gmail.com>
2019-06-14 11:43:31 -0700

    added respond_to blocks for skills_controller actions

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -4,13 +4,23 @@ class SkillsController < ApplicationController
   def index
     @target = params[:target]
     @skills = Skill.for_target(@target)
+    respond_to do |f|
+      f.html {render :index}
+      f.json {render json: @skills}
+    end
   end
 
   def show
     if @skill = Skill.find_by(name: params[:id])
     else @skill = Skill.find(params[:id])
     end
-    @video = "https://player.vimeo.com/video/" + @skill.demo.split("/").last
+    if @skill.demo != ""
+      @video = "https://player.vimeo.com/video/" + @skill.demo.split("/").last
+    end
+    respond_to do |f|
+      f.html {render :show}
+      f.json {render json: @skill}
+    end 
   end
 
   def targets
@@ -18,6 +28,10 @@ class SkillsController < ApplicationController
 
     @skills_targets = @skills.map do |skill|
       skill.target
+    end
+    respond_to do |f|
+      f.html {render :targets}
+      f.json {render json: @skills}
     end
   end
 end
